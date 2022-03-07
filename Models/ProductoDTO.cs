f7e094f5296b18335f732a442a27d2cb38985ff2
maurobernal <info@maurobernal.com.ar>
2022-03-06 22:51:40 -0300

    WSL adaptacion

--- Models/ProductoDTO.cs
+++ Models/ProductoDTO.cs
@@ -3,10 +3,10 @@ namespace MPIntegracion.Models
     public class ProductoDTO
     {
         public Guid ID { get; set; }
-        public string Titulo { get; set; }
-        public string Descripcion { get; set; }
-        public string URL { get; set; }
-        public string imagen { get; set; }
+        public string? Titulo { get; init; }=string.Empty;
+        public string? Descripcion { get; set; }=string.Empty;
+        public string? URL { get; set; }="";
+        public string? imagen { get; set; }="";
         public double Precio {get;set;}
 
     }
